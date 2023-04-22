79417f314869a09cbedc1753d168b9efb25f0664
sergio estevao <smestevao11@gmail.com>
2023-04-22 17:30:37 +0100

    feat: holidatys

--- src/holidays.py
+++ src/holidays.py
@@ -1,7 +1,7 @@
 
 nationalHolidays = []
 nationalHolidays.append("01-01") #Ano Novo
-nationalHolidays.append("Data Móvel") 	#Sexta-feira Santa
+nationalHolidays.append("Data Móvel") 	#Sexta-feira Santa + Páscoa + Segunda Feira
 nationalHolidays.append("04-25") #Revolução dos Cravos
 nationalHolidays.append("05-01") #Dia do Trabalho
 nationalHolidays.append("06-10") #Dia de Portugal, de Camões e das Comunidades Portuguesas
@@ -17,16 +17,18 @@ regionalHolidays = []
 regionalHolidays.append(("01-11", "Obidos")) # ver se acentos dão match
 regionalHolidays.append(("01-13", "Cadaval"))
 regionalHolidays.append(("01-13", "Santa Maria de Penaguião")) # ver se acentos dão match
-regionalHolidays.append(("01-13", "Viana de Alentejo"))
+regionalHolidays.append(("01-13", "Viana do Alentejo"))
 regionalHolidays.append(("01-13", "Vila Nova de Poiares"))
 regionalHolidays.append(("01-14", "Elvas"))
 regionalHolidays.append(("01-15", "Santa Cruz"))
 regionalHolidays.append(("01-20", "Santa Maria da Feira"))
 regionalHolidays.append(("01-22", "São Vicente")) # ver se acentos dão match
 regionalHolidays.append(("01-22", "Vila do Bispo"))
-regionalHolidays.append(("02-02", "Mourao")) # ver se acentos dão match
+
+regionalHolidays.append(("02-02", "Mourão")) # ver se acentos dão match
 regionalHolidays.append(("02-10", "Aguiar da Beira"))
 regionalHolidays.append(("02-18", "Valença")) # ver se acentos dão match
+
 regionalHolidays.append(("03-01", "Tomar"))
 regionalHolidays.append(("03-02", "Vila Nova de Paiva"))
 regionalHolidays.append(("03-04", "Manteigas"))
@@ -35,6 +37,12 @@ regionalHolidays.append(("03-08", "Montemor-o-Novo")) # ver se acentos dão matc
 regionalHolidays.append(("03-12", "Monção")) # ver se acentos dão match
 regionalHolidays.append(("03-19", "Póvoa de Lanhoso")) # ver se acentos dão match
 regionalHolidays.append(("03-19", "Santarém")) # ver se acentos dão match
-regionalHolidays.append(("03-19", "Torre de Moncorvo")) # ver se acentos dão match
-regionalHolidays.append(("03-19", "Vizela")) # ver se acentos dão match
+regionalHolidays.append(("03-19", "Torre de Moncorvo"))
+regionalHolidays.append(("03-19", "Vizela"))
+
+regionalHolidays.append(("04-02", "Alpiarça")) # ver se acentos dão match
+regionalHolidays.append(("04-06", "Fronteira"))
+regionalHolidays.append(("04-11", "Lagoa")) # ver se acentos dão match
+regionalHolidays.append(("04-23", "Velas"))
+regionalHolidays.append(("04-23", "Velas"))
 
